af62b0dfb8fda3434186c7260fe1bc117e514c44
Martin Vysny <mavi@vaadin.com>
2023-04-14 09:32:45 +0300

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,95 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.20"
+    `maven-publish`
+    signing
+}
+
+defaultTasks("clean", "build")
+
+group = "com.github.mvysny.kotlin-unsigned-jvm"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+kotlin {
+    explicitApi()
+}
+
+dependencies {
+    api(kotlin("stdlib-jdk11"))
+    testImplementation("com.github.mvysny.dynatest:dynatest:0.24")
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<Javadoc> {
+    isFailOnError = false
+}
+
+publishing {
+    repositories {
+        maven {
+            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.properties["ossrhUsername"] as String? ?: "Unknown user"
+                password = project.properties["ossrhPassword"] as String? ?: "Unknown user"
+            }
+        }
+    }
+    publications {
+        create("mavenJava", MavenPublication::class.java).apply {
+            groupId = project.group.toString()
+            this.artifactId = "kotlin-unsigned-jvm"
+            version = project.version.toString()
+            pom {
+                description.set("Utilities for working with unsigned values in Kotlin/JVM")
+                name.set("Kotlin-Unsigned-JVM")
+                url.set("https://github.com/mvysny/kotlin-unsigned-jvm")
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mavi")
+                        name.set("Martin Vysny")
+                        email.set("martin@vysny.me")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/mvysny/kotlin-unsigned-jvm")
+                }
+            }
+            from(components["java"])
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        // to see the exceptions of failed tests in Travis-CI console.
+        exceptionFormat = TestExceptionFormat.FULL
+    }
+}
