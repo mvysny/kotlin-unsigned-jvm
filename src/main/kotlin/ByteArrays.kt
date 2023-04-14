25519cdb159fd86fce7e92e1eb48af63b99763ea
Martin Vysny <mavi@vaadin.com>
2023-04-14 10:19:16 +0300

    Add ByteArray.getByte(), setByte(), getUByte(), setUByte()

--- src/main/kotlin/ByteArrays.kt
+++ src/main/kotlin/ByteArrays.kt
@@ -3,6 +3,85 @@
 package com.github.mvysny.unsigned
 
 /**
+ * Returns the (possibly negative) byte represented by the byte at the
+ * specified [byteOffset] in this object, in two's complement binary
+ * representation.
+ *
+ * The return value will be between -128 and 127, inclusive.
+ *
+ * The [byteOffset] must be non-negative, and
+ * less than the length of this object.
+ */
+public inline fun ByteArray.getByte(byteOffset: Int): Byte = get(byteOffset)
+
+/**
+ * Sets the byte at the specified [byteOffset] in this object to the
+ * two's complement binary representation of the specified [value], which
+ * must fit in a single byte.
+ *
+ * In other words, [value] must be between -128 and 127, inclusive.
+ *
+ * The [byteOffset] must be non-negative, and
+ * less than the length of this object.
+ */
+public inline fun ByteArray.setByte(byteOffset: Int, value: Byte) {
+    set(byteOffset, value)
+}
+
+/**
+ * Sets the byte at the specified [byteOffset] in this object to the
+ * two's complement binary representation of the specified [value], which
+ * must fit in a single byte.
+ *
+ * In other words, [value] must be between -128 and 127, inclusive.
+ *
+ * The [byteOffset] must be non-negative, and
+ * less than the length of this object.
+ */
+public inline fun ByteArray.setByte(byteOffset: Int, value: Int) {
+    set(byteOffset, value.toByte())
+}
+
+/**
+* Returns the positive integer represented by the byte at the specified
+* [byteOffset] in this object, in unsigned binary form.
+*
+* The return value will be between 0 and 255, inclusive.
+*
+* The [byteOffset] must be non-negative, and
+* less than the length of this object.
+ */
+public inline fun ByteArray.getUByte(byteOffset: Int): UByte = getByte(byteOffset).toUByte()
+
+/**
+* Sets the byte at the specified [byteOffset] in this object to the
+* unsigned binary representation of the specified [value], which must fit
+* in a single byte.
+*
+* In other words, [value] must be between 0 and 255, inclusive.
+*
+* The [byteOffset] must be non-negative, and
+* less than the length of this object.
+ */
+public inline fun ByteArray.setUByte(byteOffset: Int, value: UInt) {
+    set(byteOffset, value.toByte())
+}
+
+/**
+ * Sets the byte at the specified [byteOffset] in this object to the
+ * unsigned binary representation of the specified [value], which must fit
+ * in a single byte.
+ *
+ * In other words, [value] must be between 0 and 255, inclusive.
+ *
+ * The [byteOffset] must be non-negative, and
+ * less than the length of this object.
+ */
+public inline fun ByteArray.setUByte(byteOffset: Int, value: UByte) {
+    set(byteOffset, value.toByte())
+}
+
+/**
  * Sets the two bytes starting at the specified [byteOffset] in this
  * object to the two's complement binary representation of the specified
  * [value].
--- src/test/kotlin/ByteArrayTest.kt
+++ src/test/kotlin/ByteArrayTest.kt
@@ -5,6 +5,13 @@ import com.github.mvysny.dynatest.DynaTest
 class ByteArrayTest : DynaTest({
     test("api") {
         ByteArray(100).apply {
+            getByte(0)
+            setByte(0, 0)
+            setByte(0, 0.toByte())
+            getUByte(0)
+            setUByte(0, 0.toUByte())
+            setUByte(0, 0.toUInt())
+
             setShort(1, 0)
             setShort(1, 0, Endian.Little)
             setShort(1, 0.toShort())
