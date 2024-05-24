19d518ab9a2b518d6f851265d2dd523b01e7cc55
Martin Vysny <mavi@vaadin.com>
2024-05-24 08:13:03 +0300

    minor

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
 module com.github.mvysny.unsigned {
-    exports com.github.mvysny.unsigned;
+    exports com.github.mvysny.unsigned; // The "Package is Empty" error is OK - see Dummy.java for the reason for this
     requires kotlin.stdlib;
 }
