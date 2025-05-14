e6a55b3b7d3301b11ca17e5f8cc927430f8cc57f
Martin Vysny <mavi@vaadin.com>
2025-05-14 15:17:46 +0300

    release to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "2.1.20"
     `maven-publish`
     signing
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
 defaultTasks("clean", "build")
@@ -49,15 +50,6 @@ tasks.withType<Javadoc> {
 }
 
 publishing {
-    repositories {
-        maven {
-            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = project.properties["ossrhUsername"] as String? ?: "Unknown user"
-                password = project.properties["ossrhPassword"] as String? ?: "Unknown user"
-            }
-        }
-    }
     publications {
         create("mavenJava", MavenPublication::class.java).apply {
             groupId = project.group.toString()
@@ -92,6 +84,16 @@ publishing {
 
 signing {
     sign(publishing.publications["mavenJava"])
+}
+
+nexusPublishing {
+    repositories {
+        // see https://central.sonatype.org/publish/publish-portal-ossrh-staging-api/#configuration
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+        }
+    }
 }
 
 tasks.withType<Test> {
